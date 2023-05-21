73af2aeaaeb65cf8603395be00d2d825d64af953
Abotalhaa <as7109464@gmail.com>
2023-05-22 00:37:12 +0300

    add

---
+++ Me/search.py
@@ -0,0 +1,40 @@
+""""
+اقبل إدخال المستخدم وابحث في ملف json الذي تم تحميله من نفس dir الذي يحول ما يلي:
+# اطلب من المستخدم إدخال كلمة البحث الأساسية:
+# تحميل عينة json
+# معالجة خطأ قراءة إذن.
+# جمع نتيجة البحث رقم مفتاح المظهر
+# حفظ النتيجة في result.txt
+"""
+# اطلب من المستخدم إدخال كلمة البحث الأساسية
+
+
+# ابحث عن الكلمة الأساسية في ملف JSON
+
+
+# احفظ النتيجة في result.txt
+
+
+# def search_keyword(keyword):
+#     pass
+#     try:
+#         keyword = (input('Enter to search: '))
+#         for j in keyword:
+
+
+    # except ValueError as r:
+    #     print(r)
+    # except TypeError as e:
+    #     print(e)
+
+
+# Ask the user to input the search keyword
+
+
+# Search for the keyword in the JSON file
+
+
+# Save the result into result.txt
+
+# ---------------------------------------------
+
