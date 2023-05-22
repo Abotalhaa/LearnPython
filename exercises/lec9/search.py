dc184b7caa575ac017bf43bc3faee323e4262858
Abotalhaa <as7109464@gmail.com>
2023-05-22 21:16:08 +0300

    I don't know

--- exercises/lec9/search.py
+++ exercises/lec9/search.py
@@ -24,7 +24,7 @@ def save_results(results):
 # def load_summary():
 #     file = None
 #     try:
-#         file = open('sum.json', 'r')
+#         file = open('summary.json', 'r')
 #         summary = file.read()
 #         return summary
 #     except Exception as ex:
@@ -36,31 +36,37 @@ def save_results(results):
 def load_summary():
     try:
         with open("summary.json", "r") as file:
-            return json.load(file)
+            # return file.read()
+            return json.load(file)  # load as json dic {} key-value
     except Exception as ex:
         print(f"Something went wrong: {ex}")
 
 
 summary = load_summary()
-
+# print(summary)
 results = {}
 
 
 # Search for the keyword in the JSON file
 def search_keyword(keyword):
     count = 0
+    # for key, value in summary.items():
     for value in summary.values():
+        # value = summary[key]
+        # print(value)
+        # break
+        # items = summary[key]
         for lines in value:
-            if keyword == lines:
+            if keyword in lines:
                 count += 1
-    print(f"{keyword} : {count}")
+    print(f"{keyword}:{count}")
     results[keyword] = count
 
 
 # Ask the user to input the search keyword
 while True:
     keyword = input("Enter search keyword:\n")
-    if keyword == "done":
+    if keyword == "#":
         break
     search_keyword(keyword)
 
